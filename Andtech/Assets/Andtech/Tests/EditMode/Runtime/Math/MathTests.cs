0ec6ffaa4358ee8d8e21a8c825da3334fdaa2b2b
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-04-24 23:41:49 -0700

    Fix repeat function

--- Andtech/Assets/Andtech/Tests/EditMode/Runtime/Math/MathTests.cs
+++ Andtech/Assets/Andtech/Tests/EditMode/Runtime/Math/MathTests.cs
@@ -1,15 +1,30 @@
 ﻿using NUnit.Framework;
 
-namespace Andtech.Tests {
+namespace Andtech.Tests
+{
 
-	public static class MathTests {
+	public static class MathTests
+	{
 
 		[Test]
-		public static void TestRepeatInt() {
+		public static void TestRepeatPositive()
+		{
 			Assert.AreEqual(0, MathfA.Repeat(10, 10));
 			Assert.AreEqual(1, MathfA.Repeat(11, 10));
 			Assert.AreEqual(2, MathfA.Repeat(12, 10));
+		}
 
+		[Test]
+		public static void TestRepeatNegative()
+		{
+			Assert.AreEqual(0, MathfA.Repeat(-10, 10));
+			Assert.AreEqual(1, MathfA.Repeat(-9, 10));
+			Assert.AreEqual(2, MathfA.Repeat(-8, 10));
+		}
+
+		[Test]
+		public static void TestRepeatRange()
+		{
 			Assert.AreEqual(25, MathfA.Repeat(35, 20, 30));
 			Assert.AreEqual(26, MathfA.Repeat(36, 20, 30));
 			Assert.AreEqual(27, MathfA.Repeat(37, 20, 30));
@@ -17,16 +32,11 @@ namespace Andtech.Tests {
 			Assert.AreEqual(25, MathfA.Repeat(15, 20, 30));
 			Assert.AreEqual(26, MathfA.Repeat(16, 20, 30));
 			Assert.AreEqual(27, MathfA.Repeat(17, 20, 30));
-
-			Assert.AreEqual(0, MathfA.Repeat(1, 1));
-
-			Assert.AreEqual(0, MathfA.Repeat(0, 0));
-
-			Assert.AreEqual(0, MathfA.Repeat(-1, 1));
 		}
 
 		[Test]
-		public static void TestRepeatFloat() {
+		public static void TestRepeatFloat()
+		{
 			Assert.AreEqual(25.0F, MathfA.Repeat(15.0F, 20.0F, 30.0F));
 			Assert.AreEqual(26.0F, MathfA.Repeat(16.0F, 20.0F, 30.0F));
 			Assert.AreEqual(27.0F, MathfA.Repeat(17.0F, 20.0F, 30.0F));
