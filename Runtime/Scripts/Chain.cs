76575911e462a82d96bb3b7758c02dbe9c5bede3
AndrewMJordan <48300131+AndrewMJordan@users.noreply.github.com>
2021-04-25 22:49:40 +0000

    Deploy: eeac535 (v0.2.3)

---
+++ Runtime/Scripts/Chain.cs
@@ -0,0 +1,62 @@
+/*
+ *	Copyright (c) 2021, AndtechGames
+ *	All rights reserved.
+ *	
+ *	This source code is licensed under the BSD-style license found in the
+ *	LICENSE file in the root directory of this source tree
+ */
+
+using System.Collections.Generic;
+
+namespace Andtech
+{
+
+	/// <summary>
+	/// A sequential chain of modifiers.
+	/// </summary>
+	/// <typeparam name="T">The type of the items being modified.</typeparam>
+	public class Chain<T>
+	{
+		private readonly LinkedList<Operation> modifiers;
+
+		/// <summary>
+		/// Creates a chain with predefined modify operations.
+		/// </summary>
+		/// <param name="operations">The modify operations.</param>
+		public Chain(params Operation[] operations)
+		{
+			modifiers = new LinkedList<Operation>(operations);
+		}
+
+		/// <summary>
+		/// Adds a modifier stage to the chain.
+		/// </summary>
+		/// <param name="modifier">The modify operation.</param>
+		public void Add(Operation modifier) => modifiers.AddLast(modifier);
+
+		/// <summary>
+		/// Removes a modifier stage to the chain.
+		/// </summary>
+		/// <param name="operation">The modify operation.</param>
+		public void Remove(Operation operation) => modifiers.Remove(operation);
+
+		/// <summary>
+		/// Transforms <paramref name="input"/> through the chain.
+		/// </summary>
+		/// <param name="input">The input value.</param>
+		/// <returns>The output value.</returns>
+		public T Run(in T input)
+		{
+			var output = input;
+
+			foreach (var modifier in modifiers)
+			{
+				output = modifier(output);
+			}
+
+			return output;
+		}
+
+		public delegate T Operation(in T input);
+	}
+}
---
+++ Tests~/EditMode/Runtime/ChainTests.cs
@@ -0,0 +1,73 @@
+/*
+ *	Copyright (c) 2021, AndtechGames
+ *	All rights reserved.
+ *	
+ *	This source code is licensed under the BSD-style license found in the
+ *	LICENSE file in the root directory of this source tree
+ */
+
+using NUnit.Framework;
+
+namespace Andtech.Tests
+{
+
+	public class ChainTests
+	{
+
+		[Test]
+		public void TestTrivialChain()
+		{
+			var chain = new Chain<int>();
+
+			var result = chain.Run(10);
+
+			Assert.AreEqual(10, result);
+		}
+
+		[Test]
+		public void TestModifiedChain()
+		{
+			var chain = new Chain<int>(Subtract);
+
+			var result = chain.Run(10);
+
+			Assert.AreEqual(9, result);
+
+			int Subtract(in int input)
+			{
+				return input - 1;
+			}
+		}
+
+		[Test]
+		public void TestBooleanChain()
+		{
+			var chain = new Chain<bool>(TestAndTrue, TestAndFalse, TestAndTrue);
+			var result = chain.Run(true);
+			Assert.IsFalse(result);
+
+			chain = new Chain<bool>(Invert, Passthrough, Invert, Invert);
+			result = chain.Run(false);
+			Assert.IsFalse(false);
+
+			bool Passthrough(in bool x) {
+				return x;
+			}
+
+			bool Invert(in bool x)
+			{
+				return !x;
+			}
+
+			bool TestAndTrue(in bool x)
+			{
+				return x & true;
+			}
+
+			bool TestAndFalse(in bool x)
+			{
+				return x & false;
+			}
+		}
+	}
+}
