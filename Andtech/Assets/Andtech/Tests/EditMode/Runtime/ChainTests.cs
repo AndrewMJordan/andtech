eeac5353a3066fa62f98819b46d95ce015a29ef8
Andrew Jordan <andrewmichaeljordan@outlook.com>
2021-04-25 15:48:58 -0700

    Add chain

---
+++ Andtech/Assets/Andtech/Tests/EditMode/Runtime/ChainTests.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+
+namespace Andtech.Tests
+{
+
+	public class ChainTests
+	{
+
+		[Test]
+		public void TestTrivialChain()
+		{
+			var chain = new Chain<int>();
+
+			var result = chain.Run(10);
+
+			Assert.AreEqual(10, result);
+		}
+
+		[Test]
+		public void TestModifiedChain()
+		{
+			var chain = new Chain<int>(Subtract);
+
+			var result = chain.Run(10);
+
+			Assert.AreEqual(9, result);
+
+			int Subtract(in int input)
+			{
+				return input - 1;
+			}
+		}
+
+		[Test]
+		public void TestBooleanChain()
+		{
+			var chain = new Chain<bool>(TestAndTrue, TestAndFalse, TestAndTrue);
+			var result = chain.Run(true);
+			Assert.IsFalse(result);
+
+			chain = new Chain<bool>(Invert, Passthrough, Invert, Invert);
+			result = chain.Run(false);
+			Assert.IsFalse(false);
+
+			bool Passthrough(in bool x) {
+				return x;
+			}
+
+			bool Invert(in bool x)
+			{
+				return !x;
+			}
+
+			bool TestAndTrue(in bool x)
+			{
+				return x & true;
+			}
+
+			bool TestAndFalse(in bool x)
+			{
+				return x & false;
+			}
+		}
+	}
+}
